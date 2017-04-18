8aef10338d2c39bae44350bd69870a4866329984
James Johnson <music1384@yahoo.co.uk>
2017-04-18 06:29:35 +0100

    Implemented base Player functionality.

--- Assets/Code/Classes/PlayerController.cs
+++ Assets/Code/Classes/PlayerController.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : Entity
+{
+    [SerializeField] private float _FireRate = 0.0f;
+    [SerializeField] private PlayerProjectile _Projectile = null;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool _CanFire = true;
+
+    private void GetInput ()
+    {
+        if(Input.GetButtonDown("Fire1") && _CanFire)
+            StartCoroutine ("FireProjectile");
+
+        float x = Input.GetAxis ("Horizontal");
+        float y = Input.GetAxis ("Vertical");
+
+        //TODO: Lock player to fixed axis based on movement.
+        //TODO: Rotate player sprite.
+        Move (new Vector2 (x, y));
+    }
+
+    protected override void Move (Vector2 dir)
+    {
+        _Transform.Translate (dir * Time.deltaTime * _Speed);
+    }
+
+    private IEnumerator FireProjectile ()
+    {
+        _CanFire = false;
+
+        var go = (GameObject)Instantiate (_Projectile.gameObject, _Transform.position, Quaternion.identity);
+
+        //TODO: Implement logic with bullet and possible bullet pool.
+
+        yield return new WaitForSeconds (_FireRate);
+
+        _CanFire = true;
+    }
 }
