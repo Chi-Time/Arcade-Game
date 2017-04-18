c721aaae8d0aac87c4db563126bb7c3130d8804c
James Johnson <music1384@yahoo.co.uk>
2017-04-18 06:29:23 +0100

    Implemented base projectile functionality.

--- Assets/Code/Classes/PlayerProjectile.cs
+++ Assets/Code/Classes/PlayerProjectile.cs
@@ -2,15 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerProjectile : MonoBehaviour {
+public class PlayerProjectile : MonoBehaviour
+{
+    [Tooltip("The speed of the projectile's movement.")]
+    [SerializeField] private float _Speed = 0.0f;
+    [Tooltip("The length of time before the bullet is culled.")]
+    [SerializeField] private float _Lifetime = 0.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody2D _Rigidbody2D = null;
+    private Transform _Transform = null;
+
+    private void Awake ()
+    {
+        Initialise ();
+    }
+
+    private void Initialise ()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D> ();
+        _Transform = GetComponent<Transform> ();
+    }
+
+    private void Start ()
+    {
+        Setup ();
+    }
+
+    private void Setup ()
+    {
+        _Rigidbody2D.gravityScale = 0f;
+        _Rigidbody2D.isKinematic = true;
+        _Rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        Invoke ("Cull", _Lifetime);
+    }
+
+    private void Update ()
+    {
+        Move ();
+    }
+
+    private void Move ()
+    {
+        _Transform.Translate (_Transform.forward * Time.deltaTime * _Speed);
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        //TODO: Implement collision logic with enemies and walls.
+        if (!other.CompareTag ("Player"))
+            Cull ();
+    }
+
+    private void Cull ()
+    {
+        //TOOD: Destroy for now, consider pool eventually.
+        Destroy (this.gameObject);
+    } 
 }
