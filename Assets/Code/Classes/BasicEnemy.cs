63e1d1abdf1a65c7043b57f23ca7eee857f62c3e
James Johnson <music1384@yahoo.co.uk>
2017-04-18 06:32:18 +0100

    Formatted empty template

--- Assets/Code/Classes/BasicEnemy.cs
+++ Assets/Code/Classes/BasicEnemy.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicEnemy : MonoBehaviour {
+public class BasicEnemy : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
