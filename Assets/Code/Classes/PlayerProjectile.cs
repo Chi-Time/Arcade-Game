e8180e331b631e1cf2dccf009fbf6cffdf5900c8
James Johnson <music1384@yahoo.co.uk>
2017-04-18 06:37:57 +0100

    Ensured component was added to script

--- Assets/Code/Classes/PlayerProjectile.cs
+++ Assets/Code/Classes/PlayerProjectile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Rigidbody2D))]
 public class PlayerProjectile : MonoBehaviour
 {
     [Tooltip("The speed of the projectile's movement.")]
