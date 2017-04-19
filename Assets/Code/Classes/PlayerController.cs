7685fd45144c053bb8e975fda5d7a4ca9b6da8c3
James Johnson <music1384@yahoo.co.uk>
2017-04-19 05:04:25 +0100

    Refactored class and implemented pool for bullets

--- Assets/Code/Classes/PlayerController.cs
+++ Assets/Code/Classes/PlayerController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class PlayerController : Entity
 {
     [SerializeField] private float _FireRate = 0.0f;
-    [SerializeField] private PlayerProjectile _Projectile = null;
+    [SerializeField] private BulletPool _Pool = new BulletPool ();
 
     private bool _CanFire = true;
     private Vector2 _CurrentDirection = Vector2.up;
+
+    protected override void Initialise ()
+    {
+        base.Initialise ();
+
+        _Pool.Intialise ();
+    }
 
     private void Update ()
     {
@@ -21,10 +28,6 @@ public class PlayerController : Entity
     {
         float x = Input.GetAxis ("Horizontal");
         float y = Input.GetAxis ("Vertical");
-
-        //TODO: Lock player to fixed axis based on movement.
-        //TODO: Get angle of player sprite for projectile.
-        //TODO: Rotate player sprite.
 
         if (x > 0.0f)
             Move (Vector2.right);
@@ -46,21 +49,27 @@ public class PlayerController : Entity
     {
         _CanFire = false;
 
-        var go = (GameObject)Instantiate (_Projectile.gameObject, _Transform.position, Quaternion.identity);
+        var projectile = _Pool.RetrieveFromPool ();
 
-        if (_CurrentDirection == Vector2.right)
-            go.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range (133.5f, 136.5f));
-        else if (_CurrentDirection == Vector2.left)
-            go.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range (223.5f, 226.5f));
-        else if (_CurrentDirection == Vector2.up)
-            go.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range(-1.5f, 1.5f));
-        else if (_CurrentDirection == Vector2.down)
-            go.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range (88.5f, 91.5f));
-
-        //TODO: Implement logic with bullet and possible bullet pool.
+        if (projectile)
+            SetProjectileDirection (projectile.gameObject);
 
         yield return new WaitForSeconds (_FireRate);
 
         _CanFire = true;
+    }
+
+    private void SetProjectileDirection (GameObject projectile)
+    {
+        projectile.transform.position = _Transform.position;
+
+        if (_CurrentDirection == Vector2.right)
+            projectile.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range (133.5f, 136.5f));
+        else if (_CurrentDirection == Vector2.left)
+            projectile.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range (223.5f, 226.5f));
+        else if (_CurrentDirection == Vector2.up)
+            projectile.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range (-1.5f, 1.5f));
+        else if (_CurrentDirection == Vector2.down)
+            projectile.transform.rotation = Quaternion.Euler (0f, 0f, Random.Range (88.5f, 91.5f));
     }
 }
