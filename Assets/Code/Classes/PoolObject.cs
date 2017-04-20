f57f8180a444c29da4bdbca70ce45acee32107ae
James Johnson <music1384@yahoo.co.uk>
2017-04-20 15:34:32 +0100

    Created abstract pool object base

---
+++ Assets/Code/Classes/PoolObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PoolObject : MonoBehaviour
+{
+    protected Pool _Pool = null;
+
+    public void Setup (Pool pool)
+    {
+        _Pool = pool;
+    }
+
+    public virtual void Cull ()
+    {
+        _Pool.ReturnToPool (this);
+    }
+}
